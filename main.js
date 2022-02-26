dbec6dd12f24637e24c6d75f8f95ae9621b96d35
BryanOri <bryanorihuela@computingforall.org>
2022-02-26 12:06:10 -0800

    stuff

--- main.js
+++ main.js
@@ -5,7 +5,14 @@
 //add function for Breedlist
 //Call start function
 
+async function start(){
+    const response = await fetch(`https://dog.ceo/api/breeds/list/all`);
+    const data = await response.json();
 
+    createBreedList(data.message);
+}
+
+start();
 
 //create creatBreedList function
 //pass in breedList as parameter
@@ -14,6 +21,17 @@
 //use Object class and keys methos to map breed
 //add .join to make a list
 
+function createBreedList(breedList){
+    document.getElementById("breed").innerHTML = `
+    <select onchange="loadByBreed(this.value)">
+    <option>Choose Dog Breed</option>
+    ${Object.keys(breedList).map(function(breed){
+        return `<option> ${breed}</option>`
+    }).join(``)}
+    </select>
+    `
+
+}
 
 
 //add async function that responds to dog breed 
@@ -21,3 +39,7 @@
 //set const response equal to await fetch with api
 //set const data equal to await response.json()
 //console.log(data)
+
+async function loadByBreed(breed){
+    alert(breed);
+}
